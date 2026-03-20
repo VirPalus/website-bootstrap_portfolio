3838b7e2309360d9cc7c418fdee7fec6060576cc
VirPalus <mario_moerman@icloud.com>
2026-03-20 21:09:55 +0100

    over mij pagina

--- JS/App.js
+++ JS/App.js
@@ -2,3 +2,25 @@
 
 fetch("navbar.html").then(r => r.text()).then(h => navbar.innerHTML = h);
 fetch("footer.html").then(r => r.text()).then(h => footer.innerHTML = h);
+
+// Skill Swiper
+
+document.addEventListener("DOMContentLoaded", function () {
+    new Swiper(".skillSwiper", {
+        loop: true,
+        spaceBetween: 24,
+        slidesPerView: 1,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev"
+        },
+        breakpoints: {
+            768: {
+                slidesPerView: 2
+            },
+            992: {
+                slidesPerView: 3
+            }
+        }
+    });
+});
